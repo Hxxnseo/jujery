4b325f175cd69abf1a581852e79972bcdd39fa8c
Hxxnseo <seoh1999@naver.com>
2022-11-15 15:52:06 +0900

    11/15 15:52

--- sub/main_menu_brewery/eco_brewery/js/eco.js
+++ sub/main_menu_brewery/eco_brewery/js/eco.js
@@ -1,11 +1,1 @@
-$(function () {
-  $(".img1").on({
-    mouseenter: function () {
-      $(".img1 .store").stop().show();
-      $(".img1 .store").css("opacity", 0.5);
-    },
-    mouseleave: function () {
-      $(".img1 .store").stop().hide();
-    },
-  });
-});
+$(function () {});
