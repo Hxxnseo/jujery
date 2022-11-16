638ba97e7f1278c6e776148e82a1016f0fb11c43
Oliviaa4992 <choyujeong@gmail.com>
2022-11-17 02:40:47 +0900

    11/17 2:40

--- sub/main_menu_region/js/logomenu.js
+++ sub/main_menu_region/js/logomenu.js
@@ -3,10 +3,10 @@ $(function () {
   
     console.log(logoMenu);
   
-    $(window).on("wheel", function (e) {
+    $(window).on("scroll", function (e) {
       console.log($(document).scrollTop());
   
-      if ($(document).scrollTop() > logoMenu) {
+      if ($(document).scrollTop() > 3000) {
         $(".menu .gnb li a").css("color", "white");
         $(".logo h1 img").attr("src", "../../../../images/logo/main_white_logo.png");
       } else {
