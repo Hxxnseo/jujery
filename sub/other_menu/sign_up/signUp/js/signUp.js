7a0d231d0a2473557a832ca5a8297889484d0b09
HyeminWang <whm4040@gmail.com>
2022-11-20 22:57:59 +0900

    11/20 10:57

--- sub/other_menu/sign_up/signUp/js/signUp.js
+++ sub/other_menu/sign_up/signUp/js/signUp.js
@@ -1,22 +1,76 @@
 // 유효성검사
 function check() {
-    const frm = document.frm;
-    if(!frm.user_id.value) {
-        return false;
-    } else if (!frm.user_id.value, frm.user_pw.value, frm.user_pw2.value) {
-        return false;
-    } else if (!frm.mobile.value) {
-        return false;
-    } else if (!frm.email.value) {
-        return false;
-    } return true;
-};
+    var frm = document.frm;
+    var userId = document.getElementById("user_id");
+    var userPw = document.getElementById("user_pw");
+    var userPw2 = document.getElementById("user_pw2");
+    var phone = document.getElementById("phone");
+    var mobile = document.getElementById("mobile");
+    var email = document.getElementById("email");
 
-// 공백확인 함수
-function checkExistData(value, dataName) {
-    if(value == "") {
-        alert(dataName + "입력해주세요!");
+    if (userId.value == "") {
+        alert("아이디를 입력하세요.");
+        userId.focus();
         return false;
-    }
-    return true;
-};
+    };
+
+    if(userPw.value == "") {
+        alert("비밀번호를 입력하세요.");
+        userPw.focus();
+        return false;
+    };
+
+    var pwdCheck = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
+
+    if(!pwdCheck.test(userPw.value)) {
+        alert("비밀번호는 영문자+숫자+특수문자 조합으로 8~25자리 사용해야 합니다.");
+        userPw.focus();
+        return false;
+    };
+
+    if(userPw2.value !== userPw.value) {
+        alert("비밀번호가 일치하지 않습니다.");
+        userPw2.focus();
+        return false;
+    };
+
+    var reg = /^[0-9]+/g;
+
+    if (!reg.test(phone.value)) {
+        alert("전화번호는 숫자만 입력할 수 있습니다.");
+        phone.focus();
+        return false;
+    };
+
+    if (!reg.test(mobile.value)) {
+        alert("휴대전화는 숫자만 입력할 수 있습니다.");
+        mobile.focus();
+        return false;
+    };
+
+    if (email.value == "") {
+        alert("이메일 주소를 입력하세요.");
+        email.focus();
+        return false;
+    };
+    
+    document.frm.submit();
+}
+
+function id_check() {
+window.open("", "", "width=600, height=200, left=200, top=100");
+}
+
+function change_email() {
+    var email_add = document.getElementById("email_server1");
+    var email_sel = document.getElementById("email_server2");
+
+    var idx = email_sel.options.selectedIndex;
+    var val = email_sel.options[idx].value;
+
+    email_add.value = val;
+}
+
+function search_address() {
+    window.open("", "b", "width=600, height=300, left=200, top=100");
+}
