e2efee33d8fdf637bbdfa6596cf51bc8ede446fa
HyeminWang <whm4040@gmail.com>
2022-11-22 00:00:10 +0900

    11/21 11:59

--- sub/main_menu_type/js/main2,4_common.js
+++ sub/main_menu_type/js/main2,4_common.js
@@ -84,7 +84,7 @@ $(function () {
     $(window).on("scroll", function (e) {
     	console.log($(document).scrollTop());
 
-    	if ($(document).scrollTop() > 1800) {
+    	if ($(document).scrollTop() > 2300) {
         	$(".menu .gnb li a").css("color", "white");
         	$(".logo h1 img").attr("src", "../../../images/logo/main_white_logo.png");
       	} else {
