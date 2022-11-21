5ebbb9f8f4800f98bee7037b9f1b944e25459b3f
HyeminWang <whm4040@gmail.com>
2022-11-21 23:18:52 +0900

    11/21 11:18

--- sub/other_menu/sign_up/signUp/js/side_bar.js
+++ sub/other_menu/sign_up/signUp/js/side_bar.js
@@ -1,3 +1,4 @@
+//side bar
 $(function () {
     const logoMenu = $(".footer").offset().top;
 
@@ -6,12 +7,12 @@ $(function () {
     $(window).on("scroll", function (e) {
     	console.log($(document).scrollTop());
 
-    	if ($(document).scrollTop() > 900) {
+    	if ($(document).scrollTop() > 550) {
         	$(".menu .gnb li a").css("color", "white");
-        	$(".logo h1 img").attr("src", "../../../images/logo/main_white_logo.png");
+        	$(".logo h1 img").attr("src", "../../../../images/logo/main_white_logo.png");
       	} else {
         	$(".menu .gnb li a").css("color", "black");
-        	$(".logo h1 img").attr("src", "../../../images/logo/detail_black_logo.png");
+        	$(".logo h1 img").attr("src", "../../../../images/logo/detail_black_logo.png");
       	}
     	});
 });
