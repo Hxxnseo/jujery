518c15a5e0474870bbf7ee095531a22a7db969d6
HyeminWang <whm4040@gmail.com>
2022-11-21 23:51:56 +0900

    11/21 11:50

--- sub/other_menu/sign_up/signUp/js/id_check.js
+++ sub/other_menu/sign_up/signUp/js/id_check.js
@@ -1,6 +1,5 @@
 //아이디 중복체크 팝업창(현재 공백문서)
 function id_check() {
-    //window.open("팝업될 문서 경로", "팝업될 문서 이름", "옵션");
     window.open("", "", "width=600, height=200, left=200, top=100");
 }
 
@@ -9,7 +8,7 @@ function change_email() {
     var email_add = document.getElementById("email_server1");
     var email_sel = document.getElementById("email_server2");
 
-    //지금 골라진 옵션의 순서와 값 구하기
+//지금 골라진 옵션의 순서와 값 구하기
     var idx = email_sel.options.selectedIndex;
     var val = email_sel.options[idx].value;
 
