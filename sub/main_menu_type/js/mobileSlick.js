c292dd1873a107c4db36a7db69b9fb29303ac006
hyeminWang <whm4040@gmail.com>
2022-11-22 18:32:59 +0900

    11/22 18:32

--- sub/main_menu_type/js/mobileSlick.js
+++ sub/main_menu_type/js/mobileSlick.js
@@ -17,4 +17,17 @@ var init = {
         slider.slick("unslick");
     }
     });
+
+    // mobile menu_bar
+		$(".beforeMenu").on("click", function () {
+			$(".wrapper1").slideDown();
+			$(".beforeMenu").hide();
+			$(".afterMenu").show();
+		});
+		
+		  $(".afterMenu").on("click", function () {
+			$(".wrapper1").slideUp();
+			$(".afterMenu").hide();
+			$(".beforeMenu").show();
+		});
 });
