985d2f52714b1218f02a85a9e281cecb51c82eb7
hyeminWang <whm4040@gmail.com>
2022-11-22 19:27:58 +0900

    11/22 19:27

--- sub/main_menu_type/js/main2,4_common.js
+++ sub/main_menu_type/js/main2,4_common.js
@@ -84,7 +84,7 @@ $(function () {
     $(window).on("scroll", function (e) {
     	console.log($(document).scrollTop());
 
-    	if ($(document).scrollTop() > 2300) {
+    	if ($(document).scrollTop() > 2200) {
         	$(".menu .gnb li a").css("color", "white");
         	$(".logo h1 img").attr("src", "../../../images/logo/main_white_logo.png");
       	} else {
--- sub/main_menu_type/js/otherjoo.js
+++ sub/main_menu_type/js/otherjoo.js
@@ -59,7 +59,7 @@ $(function () {
     $(window).on("scroll", function (e) {
     	console.log($(document).scrollTop());
 
-    	if ($(document).scrollTop() > 1800) {
+    	if ($(document).scrollTop() > 2200) {
         	$(".menu .gnb li a").css("color", "white");
         	$(".logo h1 img").attr("src", "../../../images/logo/main_white_logo.png");
       	} else {
