69967e2930cd291573980728557a91ca3099f6f2
Oliviaa4992 <choyujeong@gmail.com>
2022-11-24 21:21:35 +0900

    11.24 21:20

--- sub/other_menu/privacy_policy/js/logomenu.js
+++ sub/other_menu/privacy_policy/js/logomenu.js
@@ -6,7 +6,7 @@ $(function () {
   $(window).on("scroll", function (e) {
     console.log($(document).scrollTop());
 
-    if ($(document).scrollTop() >= logoMenu-500) {
+    if ($(document).scrollTop() > 2400) {
       $(".menu .gnb li a").css("color", "white");
       $(".logo h1 img").attr(
         "src",
