0016d97975ddd460b28fe2dab47721f7a8cb6390
Oliviaa4992 <choyujeong@gmail.com>
2022-11-24 20:30:42 +0900

    11/24 20:30

---
+++ sub/other_menu/about_us/js/logomenu.js
@@ -0,0 +1,17 @@
+$(function () {
+ 
+
+  $(".beforeMenu").on("click", function () {
+    $(".mobile-img").attr("src", "./img/mobile_detail_white_logo.png")
+    $(".wrapper1").slideDown();
+    $(".beforeMenu").hide();
+    $(".afterMenu").show();
+  });
+
+  $(".afterMenu").on("click", function () {
+    $(".mobile-img").attr("src", "./img/mobile_detail_white_logo2.png")
+    $(".wrapper1").slideUp();
+    $(".afterMenu").hide();
+    $(".beforeMenu").show();
+  });
+});
