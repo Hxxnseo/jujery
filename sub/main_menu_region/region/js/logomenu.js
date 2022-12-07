863e50b41aa4f421ba6f6a9992f30eecb21b5d4f
Oliviaa4992 <choyujeong@gmail.com>
2022-12-07 15:19:41 +0900

    12/07 15:19

--- sub/main_menu_region/region/js/logomenu.js
+++ sub/main_menu_region/region/js/logomenu.js
@@ -10,13 +10,13 @@ $(function () {
       $(".menu .gnb li a").css("color", "white");
       $(".logo h1 img").attr(
         "src",
-        "../../../../images/logo/main_white_logo.png"
+        "../../../images/logo/detail_black_logo.png"
       );
     } else {
       $(".menu .gnb li a").css("color", "black");
       $(".logo h1 img").attr(
         "src",
-        "../../../../images/logo/detail_black_logo.png"
+        "../../../images/logo/detail_black_logo.png"
       );
     }
   });
