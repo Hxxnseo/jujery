fc7617ceb9d39c921bf249e42d502d6217816069
jnju <dirjinju719@gmail.com>
2023-01-18 12:24:52 +0900

    01/18 12:24

--- sub/other_menu/logo/js/logo.js
+++ sub/other_menu/logo/js/logo.js
@@ -27,27 +27,4 @@ $(function() {
         $(".logo h1 img").attr("src", "../../../images/logo/detail_black_logo.png");
     }
     });
-
-    $('.logoguide').css({'opacity':'0'});
-    
-    $('section').css('opacity', '0');
-    $(window).on("scroll", function() {
-        var sc_top = $(this).scrollTop();
-        console.log(sc_top);
-        
-        if($(this).scrollTop() > 200){
-            $('.logoguide').animate({'opacity':'1'}, 1000);
-        }
-        if($(this).scrollTop() > 800){
-            $('.button').animate({'opacity':'1'}, 1000);
-        }
-        if($(this).scrollTop() > 1000){
-            $('.snsImg').animate({'opacity':'1'}, 1000);
-        }
-        if($(this).scrollTop() > 1900){
-            $('.warning').animate({'opacity':'1'}, 1000);
-        }
-    });    
-
-    
 })
